42fb65d100c234284364a758770c017a327fa839
Estela Madariaga <estelacode@users.noreply.github.com>
2024-08-31 11:24:34 +0200

    dev: rename app folder

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from setuptools import setup
 
 
 setup(
-    name = "tag_your_music_v2", # nombre  de la app
+    name = "tag_your_music_app", # nombre  de la app
     version = "0.0.1",  # version de la app
     packages = ["src"], # paquetes que publica o utiliza
-    entry_points = {"console_scripts":["tag_your_music_v2 = src.__main__:main"]}, # puntos de entrada, por donde vamos a llamar a la aplicacion.
+    entry_points = {"console_scripts":["tag_your_music_app = src.__main__:main"]}, # puntos de entrada, por donde vamos a llamar a la aplicacion.
     install_requires = open("requirements.txt").read().splitlines() # Paquetes  que se requieren
 )
